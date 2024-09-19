5feac24cee1571ac1b284b4b6bc28fa811c13ae6
max <github.com@maxos.email>
2024-09-19 13:41:08 +0800

    Add chapter title

--- src/components/ChapterList.js
+++ src/components/ChapterList.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import _ from 'lodash';
+import React, { useState, useEffect } from "react";
+import _ from "lodash";
 
-import bookService from '../servers/bookServer';
-import NoteList from './NoteList';
-import BookNav from './BookNav';
+import bookService from "../servers/bookServer";
+import NoteList from "./NoteList";
+import BookNav from "./BookNav";
 
 const ChapterItem = ({ bookId, chapter }) => {
   if (_.isEmpty(chapter.title)) {
-    chapter.title = 'Untitled';
+    chapter.title = "Untitled";
   }
   return (
     <section id={chapter.id}>
+      <h3>{chapter.title}</h3>
       <NoteList bookId={bookId} chapterId={chapter.id} />
     </section>
   );
 };
 
-const ChapterList = ({bookId}) => {
+const ChapterList = ({ bookId }) => {
   const [chapters, setChapters] = useState([]);
 
   useEffect(() => {
-    bookService
-      .getChaptersByBookId(bookId)
-      .then((chapters) => {
-        setChapters(chapters);
-      });
+    bookService.getChaptersByBookId(bookId).then((chapters) => {
+      setChapters(chapters);
+    });
   }, []);
 
   return (
-    <div className='book-details'>
-      <div className='book-annotations'>
-        { _.isEmpty(chapters)
-          ? <li>No notes found.</li>
-          : chapters.map(chapter => <ChapterItem key={chapter.id} bookId={bookId} chapter={chapter} />)}
+    <div className="book-details">
+      <div className="book-annotations">
+        {_.isEmpty(chapters) ? (
+          <li>No notes found.</li>
+        ) : (
+          chapters.map((chapter) => (
+            <ChapterItem key={chapter.id} bookId={bookId} chapter={chapter} />
+          ))
+        )}
       </div>
-      <BookNav key={chapters.id} className='book-nav' chapters={chapters} />
+      <BookNav key={chapters.id} className="book-nav" chapters={chapters} />
     </div>
   );
 };
